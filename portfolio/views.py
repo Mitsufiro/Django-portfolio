8ca4e1f484c89a2993a5706cb027ee230d6e3d08
Ilyal <moshka119@gmail.com>
2022-06-06 19:46:27 +0300

    views path

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,9 +1,14 @@
+import os
+
 from django.shortcuts import render
+
+from personal_portfolio import settings
 from .models import Project
+
+BASE_DIR = getattr(settings, "BASE_DIR", None)
 
 
 def home(request):
     projects = Project.objects.all()
-    return render(request, 'C:\Projects\personal_portfolio-project\portfolio\\templates\portfolio\home.html',
+    return render(request, f'{os.path.join(BASE_DIR)}\portfolio\\templates\portfolio\home.html',
                   {'projects': projects})
-# Create your views here.
